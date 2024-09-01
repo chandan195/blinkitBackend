612ccc593adfca150656b3552a69157fb71737db
@chandan195 <chandanmariya98@gmail.com>
2024-09-01 11:56:42 +0530

    connect project mongoose database

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ make a application like blink it.
 
 # Fastify
 Fast and low overhead web framework, for Node.js
+  
+
+ # mongoose and  dotenv
+
+ #   tslib 
+
+ This is a runtime library for TypeScript that contains all of the TypeScript helper functions.
--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-import fastify from 'fastify';
 
 
+import "dotenv/config";
+import fastify from "fastify";
+
+import { connectDB } from "./src/config/connect.js";
 
 const start = async () => {
-    const app =fastify();
-    const Port = process.env.PORT || 3000;
-    app.listen({port:Port , host:"0.0.0.0"},(err ,addr)=>{
-        if(err) {
-            console.error(err);
-
-        }else{
-            console.log(`Blink it start on http://localhost:${Port}`);
-        }
-    });
+  await connectDB(process.env.MONGO_URI);
+  const app = fastify();
+  const Port = process.env.PORT || 3000;
+  app.listen({ port: Port, host: "0.0.0.0" }, (err, addr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Blink it start on http://localhost:${Port}`);
+    }
+  });
 };
 
 start();
