6b3c8a10f7ef5fbe087c950c35994d9280aa89f7
@chandan195 <chandanmariya98@gmail.com>
2024-09-01 12:39:01 +0530

    create schema model

--- src/config/connect.js
+++ src/config/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async(uri)=>{
     try {
        await mongoose.connect(uri);
-       console.log("DB connection established")
+       console.log("DB connection established ✅ ")
     } catch (error) {
         console.log("Database connection error",error);
     }
