10031ae5e72aa024e81307f713f0f3daed4362b5
@chandan195 <chandanmariya98@gmail.com>
2024-09-02 11:41:26 +0530

    facing problem admin configration

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ import fastify from "fastify";
 
 import { connectDB } from "./src/config/connect.js";
 import { PORT } from "./src/config/config.js";
+import { buildAdminRouter } from "./src/config/setup.js";
 
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
   const app = fastify();
  
-
-  
-  app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+await buildAdminRouter(app);
+   app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+  // app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
     if (err) {
       console.error(err);
     } else {
-      console.log(`Blink it start on http://localhost:${PORT}`);
+      console.log(`Blink it start on http://localhost:${PORT}${admin.options.rootPath}`);
     }
   });
 };
