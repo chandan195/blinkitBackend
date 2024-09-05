6a25dd1a6ab75e120c87a800beba878fc4a7fb79
@chandan195 <chandanmariya98@gmail.com>
2024-09-05 13:33:04 +0530

    delivery partner auth

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -19,20 +19,52 @@ const generateToken = (user) => {
     refreshToken,
   };
 };
- export const loginCustomer =async (req ,reply)=>{
-
-    try {
-        const {phone }= req.body;
-        let customer = await Customer.findOne({phone});
-        if (!customer){
-            customer = new Customer({
-                phone,
-                role: 'Customer',
-                isActivated: true,
-            });
-            await customer.save();
-        }
-    } catch (error) {
-        
+export const loginCustomer = async (req, reply) => {
+  try {
+    const { phone } = req.body;
+    let customer = await Customer.findOne({ phone });
+    if (!customer) {
+      customer = new Customer({
+        phone,
+        role: "Customer",
+        isActivated: true,
+      });
+      await customer.save();
     }
+    const { accessToken, refreshToken } = generateToken(customer);
+    return reply.send({
+      message: customer ? "Login successful" : "Customer created and login",
+      accessToken,
+      refreshToken,
+      customer,
+    });
+  } catch (error) {
+    return reply.status(500).send({ message: " An error occurred", error });
+  }
+};
+
+export const loginDeliveryPartner = async (req, reply) => {
+  try {
+    const { email, password } = req.body;
+    let deliveryPartner = await DeliveryPartner.findOne({ email });
+    if (!deliveryPartner) {
+      return reply.status(404).send({ message: " Delivery Partner not found", error });
+    }
+
+ const isMatch =password === deliveryPartner.password
+ if(!isMatch) {
+  return reply.status(400)
+  .send({ message: " Invalid credential", error });
  }
+
+    const { accessToken, refreshToken } = generateToken(deliveryPartner);
+    return reply.send({
+      message: "Login successful",
+      accessToken,
+      refreshToken,
+      deliveryPartner,
+    });
+  } catch (error) {
+    return reply.status(500).send({ message: " An error occurred", error });
+  }
+};
