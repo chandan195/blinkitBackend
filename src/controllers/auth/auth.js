c482559f1e0087ad48abd77aa5dfbe7de138480a
@chandan195 <chandanmariya98@gmail.com>
2024-09-05 13:55:02 +0530

    refresh token validation

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -48,14 +48,15 @@ export const loginDeliveryPartner = async (req, reply) => {
     const { email, password } = req.body;
     let deliveryPartner = await DeliveryPartner.findOne({ email });
     if (!deliveryPartner) {
-      return reply.status(404).send({ message: " Delivery Partner not found", error });
+      return reply
+        .status(404)
+        .send({ message: " Delivery Partner not found", error });
     }
 
- const isMatch =password === deliveryPartner.password
- if(!isMatch) {
-  return reply.status(400)
-  .send({ message: " Invalid credential", error });
- }
+    const isMatch = password === deliveryPartner.password;
+    if (!isMatch) {
+      return reply.status(400).send({ message: " Invalid credential", error });
+    }
 
     const { accessToken, refreshToken } = generateToken(deliveryPartner);
     return reply.send({
@@ -66,5 +67,36 @@ export const loginDeliveryPartner = async (req, reply) => {
     });
   } catch (error) {
     return reply.status(500).send({ message: " An error occurred", error });
+  }
+};
+
+export const refreshToken = async (req, reply) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) {
+    return reply.status(401).send({ message: "Refresh token is required" });
+  }
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    let user ;
+    
+    if(decoded.role==="Customer") {
+      user =await Customer.findById(decoded.userId);
+
+    } else if(decoded.role==="DeliveryPartner"){
+      user =await DeliveryPartner.findById(decoded.userId);
+    } else {
+      return reply.status(403).send({ message: "Invalid role", error });
+    }
+
+  if(!user) {
+    return reply.status(403).send({ message: "Invalid refresh token", error });
+  }
+
+  const { accessToken , refreshToken :newRefreshToken } = generateToken(user);
+return reply.send({ message:"Refresh token is ", accessToken, refreshToken:newRefreshToken})
+
+  } catch (error) {
+    return reply.status(403).send({ message: "Invalid refresh token", error });
   }
 };
