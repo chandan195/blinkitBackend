bfe515286728f9eff4f9f91e1123bc3137e0b809
@chandan195 <chandanmariya98@gmail.com>
2024-09-06 18:29:36 +0530

    controller for confirm order

--- src/controllers/order/order.js
+++ src/controllers/order/order.js
@@ -1,47 +1,42 @@
 import { Order } from "../../model/order.js";
 import { Branch } from "../../model/branch.js";
-import { Customer } from "../../model/user.js";
-
+import { Customer, DeliveryPartner } from "../../model/user.js";
 
 export const createOrder = async (req, reply) => {
   try {
-    const {userId} =req.user;
-    const {items ,branch,totalPrice} =req.body;
-const customerData = await Customer.findById(userId);
-const branchData = await Branch.find(branch);
+    const { userId } = req.user;
+    const { items, branch, totalPrice } = req.body;
+    const customerData = await Customer.findById(userId);
+    const branchData = await Branch.find(branch);
 
-if(!customerData){
-    return reply.status(404).send({message:"Customer not found"});
+    if (!customerData) {
+      return reply.status(404).send({ message: "Customer not found" });
+    }
 
+    const newOrder = new Order({
+      Customer: userId,
+      items: items.map((items) => ({
+        id: items.id,
+        item: items.item,
+        count: items.count,
+      })),
+      branch,
+      totalPrice,
+      deliveryLocation: {
+        latitude: customerData.liveLocation.latitude,
+        longitude: customerData.liveLocation.longitude,
+        address: customerData.address || "NO Address available",
+      },
 
-}
+      pickupLocation: {
+        latitude: branchData.liveLocation.latitude,
+        longitude: branchData.liveLocation.longitude,
+        address: branchData.address || "NO Address available",
+      },
+    });
 
-const newOrder = new Order({
-    Customer:userId,
-    items:items.map((items)=>({
-        id:items.id,
-        item:items.item,
-        count:items.count
-    })),
-branch,
-totalPrice,
-deliveryLocation:{
-    latitude:customerData.liveLocation.latitude,
-    longitude: customerData.liveLocation.longitude,
-    address:customerData.address ||"NO Address available"
-},
-
-pickupLocation:{
-    latitude:branchData.liveLocation.latitude,
-    longitude: branchData.liveLocation.longitude,
-    address:branchData.address ||"NO Address available"
-}
-});
-
-const savedOrder = await newOrder.save();
-return reply.status(201).send(savedOrder)
-
-
+    const savedOrder = await newOrder.save();
+    return reply.status(201).send(savedOrder);
   } catch (error) {
     return reply
       .status(500)
@@ -49,6 +44,43 @@ return reply.status(201).send(savedOrder)
   }
 };
 
+export const confirmOrder = async (req, reply) => {
+  try {
+    const { orderId } = req.params;
+    const { userId } = req.user;
+    const { deliveryPersonLocation } = req.body;
 
+    const deliveryPerson = await DeliveryPartner.findById(userId);
+    if (!deliveryPerson) {
+      return reply.status(404).send({ message: "Delivery Person not found" });
+    }
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return reply.status(404).send({ message: "Order not found" });
+    }
+
+    if (order.status !== "available") {
+      return reply.status(400).send({ message: " Order not available" });
+    }
+
+    order.status = "confirmed";
+
+    order.deliveryPerson = userId;
+    order.deliveryPersonLocation = {
+      latitude: deliveryPersonLocation?.latitude,
+      longitude: deliveryPersonLocation?.longitude,
+      address: deliveryPersonLocation.address || "",
+    };
+
+    await order.save();
+
+    return reply.send(order);
+  } catch (error) {
+    return reply
+      .status(500)
+      .send({ message: "Failed to confirm order", error: error });
+  }
+};
 
 
